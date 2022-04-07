11b4b23e6ab0359ec1f80b683aab7025d7e97228
Paul Matar <paul_matar@icloud.com>
2022-04-07 23:11:48 +0300

    Initial Commit

---
+++ DuckApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DuckApp
+//
+//  Created by Paul Matar on 07.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
